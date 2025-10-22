00f970498eb7ec452010aa3b3527d0f58cb57d1c
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-10-22 08:02:29 -0600

    Create footer and header components

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container my-5 d-flex justify-content-center">
+    <div class="border rounded shadow p-3 d-flex justify-content-center
+      align-items-center gap-3 w-100 flex-wrap">
+      <img
+        :src="Credits"
+        alt="Foto de perfil"
+        class="rounded-circle me-3"
+        style="width: 60px; height: 60px; object-fit: cover;"
+      />
+      
+      <p class="mb-0 align-self-center">
+        Créditos: María Fernanda Herrera Gutiérrez</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Credits from '../assets/images/CreditsMafer.jpg';
+  export default {
+    name: 'CreditsFooter',
+    data() {
+      return {
+       Credits
+      };
+    }
+  }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container my-5 d-flex justify-content-center">
+    <div class="border rounded shadow p-3 d-flex justify-content-center
+      align-items-center gap-3 w-100 flex-wrap">
+      <img
+          :src="LogoUCR"
+          alt="UCR logo"
+          class="img-fluid"
+          style="max-width: 200px" />
+
+      <img
+          :src="LogoELM"
+          alt="ELM Logo"
+          class="img-fluid"
+          style="max-width: 200px"/>
+
+      <img
+          :src="LogoTCU"
+          alt="TCU Logo"
+          class="img-fluid"
+          style="max-width: 200px" />
+    </div>
+  </div>
+</template>
+
+<script>
+import LogoUCR from '../assets/images/LogoUCR.jpg';
+import LogoTCU from '../assets/images/LogoTCU.jpg';
+import LogoELM from '../assets/images/LogoELM.png';
+
+export default {
+  name: 'HeaderHomePage',
+  data() {
+    return {
+      LogoUCR,
+      LogoELM,
+      LogoTCU,
+    };
+  }
+}
+</script>
